8b0efab7d929424eb9bba371ced041bcb4058bf2
Mayank Gupta <mayankgupta14042000@gmail.com>
2023-04-25 01:05:47 +0530

    reload prompt

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,15 @@ import FormProvider from './context/form'
 import NodeProvider from './context/node'
 import './index.css'
 
+window.addEventListener('beforeunload', (event) => {
+  // event.preventDefault();
+  if (localStorage.getItem('modelState') != undefined && localStorage.getItem('modelState') != null) {
+    return undefined;
+  } else {
+    event.returnValue = 'Changes you made may not be saved.';
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <NodeProvider>
     <FormProvider>
